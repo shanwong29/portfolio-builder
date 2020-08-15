401c376b8588f929d6396eda0d1c76b3d09c9b4d
Shan Wong <shan.wong29@gmail.com>
2020-08-15 19:16:33 +0200

    Amend firebase function

--- functions/index.js
+++ functions/index.js
@@ -5,26 +5,40 @@ admin.initializeApp();
 
 // create function
 // addAdminRole is the name of the fn
-exports.addAdminRole = functions.https.onCall((data, context) => {
-  //get user and add custom claim (admin)
+exports.addAdminRole = functions.https.onCall(async (data, context) => {
   // context contain info about the user ==> so that can be used to check if user is admin
 
   //   if (context.auth.token.admin !== true) {
   //     return { error: "only admin can add other admins" };
   //   }
 
-  return admin
-    .auth()
-    .getUserByEmail(data.email)
-    .then((user) => {
-      return admin.auth().setCustomUserClaims(user.uid, { admin: true });
-    })
-    .then(() => {
+  const docRef = admin
+    .firestore()
+    .collection("personalInfo")
+    .doc("about");
+
+  try {
+    const doc = await docRef.get();
+    if (doc.exists) {
       return {
-        message: `Success! ${data.email} has been made as admin.`,
+        error: "App has been initialized, no re-initialization is allowed.",
       };
-    })
-    .catch((err) => {
-      console.log(err);
+    }
+
+    // get user and add custom claim (admin)
+    const user = await admin.auth().getUserByEmail(data.email);
+    await admin.auth().setCustomUserClaims(user.uid, { admin: true });
+
+    // create user collection
+    await docRef.set({
+      name: data.name,
+      interests: [],
     });
+
+    return {
+      message: `Success! ${data.email} has been made as admin.`,
+    };
+  } catch (err) {
+    console.log(err);
+  }
 });
