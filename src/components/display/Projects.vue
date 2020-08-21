a16073aff9d09c8b888704f61754d28b816cd8c6
Shan Wong <shan.wong29@gmail.com>
2020-08-21 21:42:23 +0200

    Add project stacks filter

--- src/components/display/Projects.vue
+++ src/components/display/Projects.vue
@@ -1,6 +1,28 @@
 <template>
   <div id="projects" class="py-16">
     <h1>Projects</h1>
+    <div class="d-flex justify-center align-center mt-8 mb-4">
+      <v-sheet max-width="800">
+        <v-slide-group v-model="model" center-active show-arrows>
+          <v-slide-item
+            v-for="(stack, index) in getUsedTechStacks"
+            :key="index"
+            v-slot:default="{ active}"
+          >
+            <v-btn
+              :color="active ? 'primary' : 'blue-grey lighten-5'"
+              class="ma-1"
+              rounded
+              small
+              @click="setChosenStack(index, stack)"
+            >{{ stack}}</v-btn>
+          </v-slide-item>
+        </v-slide-group>
+      </v-sheet>
+      <v-btn icon @click="clearTechStacksFilter">
+        <v-icon>mdi-close-circle</v-icon>
+      </v-btn>
+    </div>
 
     <carousel
       :navigationEnabled="true"
@@ -11,7 +33,7 @@
       paginationActiveColor="pink"
     >
       <slide
-        v-for="(project) in getShownProjects"
+        v-for="project in getShownProjects"
         :key="project.id"
         :style="{ width: getCardWidth + '%' }"
         class="slide"
@@ -80,12 +102,20 @@
 <script>
 import { mapGetters, mapState } from "vuex";
 import { Carousel, Slide } from "vue-carousel";
+import { db } from "../../firebase/init";
 
 export default {
   name: "Projects",
   components: {
     Carousel,
     Slide
+  },
+  data() {
+    return {
+      chosenStack: "",
+      idListOfProjectsWithChosenStack: [],
+      model: null
+    };
   },
   computed: {
     ...mapGetters(["filteredGithubData"]),
@@ -106,20 +136,69 @@ export default {
     },
     getShownProjects() {
       const shownProjects = this.filteredGithubData.filter(el => {
+        let hasChosenStack = true;
+
+        if (
+          this.idListOfProjectsWithChosenStack.length &&
+          this.idListOfProjectsWithChosenStack.indexOf(el.id.toString()) < 0
+        ) {
+          hasChosenStack = false;
+        }
         return (
-          !this.dbProjectsData[el.id] ||
-          this.dbProjectsData[el.id]["hide"] != true
+          this.dbProjectsData[el.id] &&
+          this.dbProjectsData[el.id]["show"] == true &&
+          hasChosenStack
         );
       });
       return shownProjects;
+    },
+    getUsedTechStacks() {
+      let allTechStacks = [];
+      for (const project in this.dbProjectsData) {
+        if (
+          this.dbProjectsData[project]["stacks"] &&
+          this.dbProjectsData[project]["show"]
+        ) {
+          allTechStacks = allTechStacks.concat(
+            this.dbProjectsData[project]["stacks"]
+          );
+        }
+      }
+
+      const noDuplicatedStacks = new Set(allTechStacks);
+      return Array.from(noDuplicatedStacks);
     }
   },
-  methods: {
-    getSlideClass(index) {
-      if (index) {
-        return "slide activeSlide";
+
+  watch: {
+    async chosenStack() {
+      if (this.chosenStack) {
+        const projects = db.collection("projects");
+
+        const snapahot = await projects
+          .where("stacks", "array-contains", this.chosenStack)
+          .get();
+
+        const projectsIdList = [];
+        snapahot.forEach(doc => {
+          projectsIdList.push(doc.id);
+        });
+
+        this.idListOfProjectsWithChosenStack = projectsIdList;
+      } else {
+        this.idListOfProjectsWithChosenStack = [];
       }
-      return "slide";
+    }
+  },
+
+  methods: {
+    clearTechStacksFilter() {
+      this.model = null;
+      this.chosenStack = "";
+    },
+    setChosenStack(index, stack) {
+      this.model = index;
+      this.chosenStack = stack;
     },
     getCoverUrl(projectId) {
       if (
